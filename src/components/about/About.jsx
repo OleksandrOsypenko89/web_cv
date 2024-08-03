fb8ed50438d36111bd2671d8ec31ee21a502740c
oleksandrosypenko <Oleksandrosypenko1989@gmail.com>
2024-08-03 13:13:27 +0200

    New link download my cv

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -5,7 +5,7 @@ export const About = () => {
     /**
      * https://drive.google.com/file/d/--------------id-------------------/view?usp=drive_link
      */
-    const _ID_PDF = '1BK70aKRMvpvFTBusnuz4eJEg_XPujKH3';
+    const _ID_PDF = '1EPmxf6YeNduLTW2N62Oe-jSwy67f9U4C';
     const _URL = `https://drive.usercontent.google.com/u/0/uc?id=${_ID_PDF}&export=download`;
 
     const handleDownload = () => {
